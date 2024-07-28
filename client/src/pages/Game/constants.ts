3badea1389bb74f152fe3c730a8db9599df73a90
hoangy761 <100112305+hoangy761@users.noreply.github.com>
2024-07-28 13:39:04 +0700

    fix: screen game

--- client/src/pages/Game/constants.ts
+++ client/src/pages/Game/constants.ts
@@ -1,10 +1,10 @@
 // game is note see full
-export const GAME_WIDTH = 2000;
+export const GAME_WIDTH = 3200;
 // const GAME_HEIGHT = 5000;
 
 //screen is visible
-export const SCREEN_WIDTH = 700;
-export const SCREEN_HEIGHT = 300;
+export const SCREEN_WIDTH = screen.width;
+export const SCREEN_HEIGHT = screen.height;
 
 export const SNAKE_SPEED = 4;
 
