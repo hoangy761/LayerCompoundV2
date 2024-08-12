8c17e3154eb7e5b305f0ee51e2aee219882d3728
hoangy761 <100112305+hoangy761@users.noreply.github.com>
2024-08-12 21:42:03 +0700

    update: server url

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,4 +1,5 @@
-const SERVER_URL = 'http://localhost:3000';
+// const SERVER_URL = 'http://localhost:3000';
+const SERVER_URL = 'https://layercompoundv2.onrender.com';
 // const SERVER_URL_USER = process.env.REACT_APP_SERVER_URL_USER;
 
 export { SERVER_URL };
