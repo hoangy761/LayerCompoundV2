669c820e7e4185bd970dabd0056c3461fe2a7bca
hoangy761 <100112305+hoangy761@users.noreply.github.com>
2024-08-13 23:04:23 +0700

    update: SCREEN

--- client/src/pages/GameV2/constants/constants.ts
+++ client/src/pages/GameV2/constants/constants.ts
@@ -3,10 +3,10 @@ export const GAME_WIDTH = 3200;
 // const GAME_HEIGHT = 5000;
 
 //screen is visible
-export const SCREEN_WIDTH = 1000;
-export const SCREEN_HEIGHT = 500;
-// export const SCREEN_WIDTH = screen.width;
-// export const SCREEN_HEIGHT = screen.height;
+// export const SCREEN_WIDTH = 1000;
+// export const SCREEN_HEIGHT = 500;
+export const SCREEN_WIDTH = screen.width;
+export const SCREEN_HEIGHT = screen.height;
 
 //miniMap width
 export const MINI_MAP_GAME_WIDTH = GAME_WIDTH / 20;
